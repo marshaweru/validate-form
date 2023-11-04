c22cbf1fad8a146fd1b2075a0f324e3e48f5559c
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-04 06:57:53 -0700

    Add function that shall validate input box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 //Add variables for IDs in html
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitErro = document.getElementById('sbmit-error');
+
+//Add function that will validate input box
