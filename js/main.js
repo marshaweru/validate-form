9c0cb491ab8cd8a15ec33e597bd815cde0600dfc
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-05 22:37:09 -0800

    First character should be an alphabetical

--- js/main.js
+++ js/main.js
@@ -10,4 +10,12 @@ function validateName(){//Function to be executed when we type into input field
     var name = document.getElementById('contact-name').value;
     //var called name will store content written in input box
     //Then add condition
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)){
+        //First character should be an alphabetical letter and every other character should be between a and z
+
+    })
 }
