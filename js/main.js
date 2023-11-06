5c5a6181d2c8c49808ac1947fdf88d4b0c30e4ad
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-05 22:25:15 -0800

    Add condition

--- js/main.js
+++ js/main.js
@@ -9,4 +9,5 @@ var submitErro = document.getElementById('sbmit-error');
 function validateName(){//Function to be executed when we type into input field
     var name = document.getElementById('contact-name').value;
     //var called name will store content written in input box
+    //Then add condition
 }
